9d76b2da6d489b9382d300e91688cf41ea10bc6b
xosion <xosion@live.cn>
2016-09-25 22:29:08 +0800

    rebuild init Request

--- src/G/framework/net/httpd/HTTPRequest.cpp
+++ src/G/framework/net/httpd/HTTPRequest.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "G/GUtil.hpp"
 #include "G/net/http/Request.hpp"
+#include "G/net/URL.hpp"
 
 using namespace std;
 using namespace G;
@@ -12,6 +13,7 @@ int HTTPRequest::init(HTTPRequest *request, StreamIO *fh)
     int tmpFlag;
     string tmp, str, data, firstRow;
     vector<string> foo, bar;
+    URL uri;
 
     tmp = fh->gets(tmp, CRLF);
     split(foo, tmp, " ");    /* 获取第一行 'GET URI HTTP/1.1' */
@@ -22,7 +24,7 @@ int HTTPRequest::init(HTTPRequest *request, StreamIO *fh)
     // }
     request->set("method", foo[0]);
     request->set("protocol", foo[2]);
-    tmp = foo[1];
+    URL::parse(&uri, foo[1]);
     while (1)    /* 设定从第二行开始的每一行 */
     {
         str = fh->gets(str, CRLF);
@@ -31,63 +33,8 @@ int HTTPRequest::init(HTTPRequest *request, StreamIO *fh)
         split(foo, str, ": ");
         request->set(foo[0], foo[1]);
     }
-    tmpFlag = 0;  /* 0 path, 1 args, 2 hash */
-    for (unsigned int i=0; i<tmp.length(); i++)
-    {
-        if ('?' == tmp[i] || '#' == tmp[i])
-        {
-            str = tmp.substr(0, i);
-            request->set("path", str);
-            if ('?' == tmp[i])
-            {
-                tmpFlag = 1;
-                tmp = tmp.erase(0, i+1);
-            }
-            else
-            {
-                tmpFlag = 2;
-            }
-            break;
-        }
-    }
-    if (0 == tmpFlag)
-    {
-        request->set("path", tmp);
-    }
-    if (1 == tmpFlag)  /* set query */
-    {
-        int begin = 0;
-        int kvFlag = 0;
-        string key;
-        unsigned int i;
-        for (i=0; i<tmp.length(); i++)
-        {
-            switch (tmp[i])
-            {
-                case '=':
-                    key = tmp.substr(begin, i-begin);
-                    kvFlag = 1;
-                    break;
-                case '&':
-                    if(kvFlag)
-                    {
-                        request->_GET.set(key, tmp.substr(begin, i-begin));
-                        kvFlag = 0;
-                    }
-                    break;
-                default:
-                    continue;
-            }
-            begin = i +1;
-        }
-        if (kvFlag)
-        {
-            if (0 == i-begin)
-                request->_GET.set(key, "");
-            else
-                request->_GET.set(key, tmp.substr(begin, i-begin));
-        }
-    }
+    request->set("path", uri.getPath());
+    // todo
     return 0;
 }
 
