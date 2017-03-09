51b31f27b2fd7ba82269a3b5b4d8bad403e00df4
wangxingzhuo <hamishwatson@live.com>
2017-03-09 23:00:20 +0800

    unix socket

--- include/G/net/UNIXServer.hpp
+++ include/G/net/UNIXServer.hpp
@@ -1,19 +1,23 @@
 #ifndef _GUNIXSERVER_HPP_
 #define _GUNIXSERVER_HPP_
 
+#include <string>
 #include "G/net/Server.hpp"
 
 namespace G {
 
     class UNIXServer: public G::Server
     {
-        virtual int service(G::IOEvents *, int max);
+        std::string path;
     public:
-        TCPServer();
-        virtual ~TCPServer();
+        UNIXServer();
+        UNIXServer(const char *path);
+        UNIXServer(std::string &path);
+        virtual ~UNIXServer();
         virtual int initPool(int, int, int);
-        int setPath(char *);
-        int service(G::IOEvents *, int max, int sslFlag);
+        int setPath(const char *);
+        int setPath(std::string&);
+        virtual int service(G::IOEvents *, int max);
     };
 
 }
