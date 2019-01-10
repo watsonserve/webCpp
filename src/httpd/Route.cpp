1001bbfddb4ba76c8b8372fef60c75bb6b844415
wangxingzhuo <hamishwatson@live.com>
2019-01-10 23:46:59 +0800

    remove print

--- src/httpd/Route.cpp
+++ src/httpd/Route.cpp
@@ -5,9 +5,7 @@
 //  Created by 王兴卓 on 16/6/11.
 //  Copyright © 2016年 watsserve. All rights reserved.
 //
-extern "C" {
-#include <stdio.h>
-}
+
 #include "Route.hpp"
 
 int Route::call(HTTPRequest *req, HTTPResponse *res)
@@ -15,11 +13,9 @@ int Route::call(HTTPRequest *req, HTTPResponse *res)
     std::string path = req->get("path");
     if("/" == path || "/index.json" == path)
     {
-        puts(path.c_str());
         res->setContent("{\"hello\": \"world\"}\r\n");
         res->end();
         return 1;
     }
-    printf("fine");
     return 0;
 }
