d6b0af706dc9706abe32781d2b991be8fe92492f
james <hamishwatson@live.com>
2020-01-16 23:17:27 +0800

    io handler

---
+++ include/G/io/IOHandler.hpp
@@ -0,0 +1,43 @@
+//
+//  IOHandle.hpp
+//  GHTTPd
+//
+//  Created by JamesWatson on 16/5/9.
+//  Copyright © 2016年 watsserve. All rights reserved.
+//
+
+#ifndef _IOHANDLE_HPP_
+#define _IOHANDLE_HPP_
+
+#include <string>
+#include "G/Object.hpp"
+#include "G/Exception.hpp"
+
+namespace G {
+
+    class FDHandler
+    {
+    public:
+        virtual void onOpen(int fd) =0;
+        virtual void onError(Exception &err) =0;
+        virtual void onClose() =0;
+    };
+
+    class InputHandler : public virtual FDHandler
+    {
+    public:
+        virtual void onData(Input &) =0;
+    };
+
+    class OutputHandler : public virtual FDHandler
+    {
+    public:
+        virtual void onWritten(Output &, int) =0;
+    };
+
+    class IOHandler : virtual public InputHandler, virtual public OutputHandler
+    {
+    };
+}
+
+#endif /* _IOHandle_h_ */
