9ab468549b9448d27fded6be3c4b67dfd51bb6b2
james <hamishwatson@live.com>
2020-03-28 21:05:50 +0800

    test server

--- test/test_server/MsgServer.cpp
+++ test/test_server/MsgServer.cpp
@@ -37,21 +37,36 @@ void MsgServer::onData(IOStream *stream)
     len = stream->read(buf, BUFSIZ);
     if (len < 0)
     {
-        printf("msgServer onData exception: %s\n", strerror(errno));
+        printf("msgServer onData: %d exception: %s\n", errno, strerror(errno));
         stream->close();
     }
     if (len < 1)
     {
+        printf("msgServer onData len < 1\n");
         return;
     }
     buf[len] = 0;
     printf("%s-------- over\n", buf);
-    strcpy(buf, "HTTP/1.1 200 OK\r\nContent-Length: 53\r\n\r\n<!DOCTYPE html><html><body>Hello world!</body></html>\0");
-    stream->write(buf, 94);
+    strcpy(buf, "HTTP/1.1 404 NOT FOUND\r\nContent-Length: 54\r\n\r\n<!DOCTYPE html><html><body>404 Not Found</body></html>\0");
+    stream->write(buf, 101);
 }
 
 void MsgServer::onWritten(IOStream *stream)
 {
-    printf("close!!\n");
-    stream->close();
+    printf("written\n");
+    char buf[BUFSIZ];
+    int len;
+
+    len = stream->read(buf, BUFSIZ);
+    if (len < 0)
+    {
+        printf("msgServer onWritten debug: %s\n", strerror(errno));
+        stream->close();
+        return;
+    }
+    if (len)
+    {
+        printf("msgServer onWritten found: %s\n", buf);
+    }
+    // stream->close();
 }
